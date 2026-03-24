860959284e2c9de66114aaf63383190dc3b89c82
Dorian Devois <dorian.devois@gmail.com>
2026-03-24 16:09:20 +0400

    fix refs

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { handlebarsHelpers } from './src/js/helpers/handlebars/index.js';
 import { globalContext } from './src/data/create-context';
 
 export default defineConfig({
+  base: '/vituum-project/',
   plugins: [
     vituum(),
     handlebars({
@@ -19,48 +20,6 @@ export default defineConfig({
       },
       // 👇 ТУТ ДОДАЄМО HELPERS
       helpers: handlebarsHelpers,
-      // helpers: {
-      //   fallback: (value, defaultValue) => {
-      //     return value || defaultValue;
-      //   },
-      //   buildUrl: (base, slug) => {
-      //     if (!base && !slug) {
-      //       return '';
-      //     }
-
-      //     const safeBase = (base || '').toString();
-      //     const safeSlug = (slug || '').toString();
-
-      //     const normalizedBase = safeBase.replace(/\/+$/, '');
-      //     const normalizedSlug = safeSlug.replace(/^\/+|\/+$/g, '');
-
-      //     if (!normalizedBase && !normalizedSlug) {
-      //       return '';
-      //     }
-
-      //     if (!normalizedBase) {
-      //       return `/${normalizedSlug}/`;
-      //     }
-      //     if (!normalizedSlug) {
-      //       return `${normalizedBase}/`;
-      //     }
-
-      //     return `${normalizedBase}/${normalizedSlug}/`;
-      //   },
-      //   absoluteUrl: (base, path) => {
-      //     if (!path) return '';
-
-      //     // якщо вже абсолютний URL — нічого не робимо
-      //     if (/^https?:\/\//i.test(path)) {
-      //       return path;
-      //     }
-
-      //     const normalizedBase = base.replace(/\/+$/, '');
-      //     const normalizedPath = path.replace(/^\/+/, '');
-
-      //     return `${normalizedBase}/${normalizedPath}`;
-      //   },
-      // },
     }),
   ],
 });
